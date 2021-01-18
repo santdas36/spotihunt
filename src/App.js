dd7a379771090f1c9b95de341f53a32645dda056
santdas36 <santdas36@gmail.com>
2021-01-18 20:47:45 +0530

    registration form v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="app">
-      <h1>Heyoo</h1>
+      <Register/>
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,85 @@
+import './Register.css';
+
+function Register() {
+  return(
+	<div className="register">
+		<div className="register__left">
+			<span>
+				christ keng. presents,
+				<h1>Spot-i-hunt</h1>
+			</span>
+			<span>
+				<p style={{margin-bottom: 1.5rem}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+				<p>You can register your team here. If you've already registered, go chill to your favorite playlist, while keeping an eye out on your inbox and our social media pages.</p>
+			</span>
+			<span>
+				<h3>Contact</h3>
+				<p>someone@gmail.com</p>
+				<p>+91 98765 432 01</p>
+			</span>
+		</div>
+		<div className="register__right">
+			<form>
+				<h2>Register Your Team</h2>
+				<div className="form__inner">
+					<div className="form__split">
+						<div className="input__field">
+							<label for="teamname"><h3>Create a name for your team</h3></label>
+							<span className="info">Info: Team name should only contain alphabets, with no spaces and numbers. Be unique. Be creative.</span>
+							<input id="teamname" />
+						</div>
+						<div className="input__field">
+							<label for="name1">Your Name</label>
+							<input id="name1" />
+						</div>
+						<div className="input__field">
+							<label for="email1">Your Email Address</label>
+							<input id="email1" type="email"/>
+							<span className="info bottom">Note: Your team members will be able login to the contest only using this email or the team name itself.</span>
+						</div>
+						<div className="input__field">
+							<label for="password">Pick a Password</label>
+							<input id="password" type="password"/>
+						</div>
+						<div className="input__field">
+							<label for="contact">Contact Number</label>
+							<input id="contact" type="tel"/>
+						</div>
+					</div>
+					<div className="form__split">
+						<div className="input__field" style="margin-bottom: 1rem;">
+							<label for="teamname"><h3>Add Members to Your Team</h3></label>
+							<span className="info">Your team can consist of utmost three participants (including you). </span>
+						</div>
+						<div className="member" data-text="Add a 2nd participant">
+							<h3>Participant #2</h3>
+								<div className="input__field">
+									<label for="name2">Name</label>
+									<input id="name2" type="text"/>
+								</div>
+								<div className="input__field">
+									<label for="email2">Email Address</label>
+									<input id="email2" type="email"/>
+								</div>
+							</div>
+							<div className="member" data-text="Add a 3rd participant">
+								<h3>Participant #3</h3>
+								<div className="input__field">
+									<label for="name3">Name</label>
+									<input id="name3" type="text"/>
+								</div>
+							<div className="input__field">
+									<label for="email3">Email Address</label>
+									<input id="email3" type="email"/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<button type="submit">Create Team</button>
+			</form>
+		</div>
+	</div>
+  );
+}
+
+export default Register;
