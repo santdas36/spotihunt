7a3c42ec0c221236aec4c0acd386d297bd892162
santdas36 <santdas36@gmail.com>
2021-01-18 21:39:59 +0530

    push

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ import Register from './components/Register';
 function App() {
   return (
     <div className="app">
-    	<ReactPlayer
-          className='react-player'
-          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-          width='100%'
-          height='100%'
-          playing
-          loop
-        />
       <Register/>
     </div>
   );
