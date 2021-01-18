fe9d94704eb0a3d69b02c23a16efe2d7485325f8
santdas36 <santdas36@gmail.com>
2021-01-18 12:54:48 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9gyVJKR3e9bE9gXRsKQ_Bdl2EejJKMGE",
+  authDomain: "spot-i-hunt.firebaseapp.com",
+  projectId: "spot-i-hunt",
+  storageBucket: "spot-i-hunt.appspot.com",
+  messagingSenderId: "555769904290",
+  appId: "1:555769904290:web:0157604e6136295d89ebfb",
+  measurementId: "G-CTR51E85X7"
+};
+
+const firebaseAdmin = firebase.initializeApp(firebaseConfig);
+const db = firebaseAdmin.firestore();
+const auth = firebase.auth();
+export { auth };
+export default db;
