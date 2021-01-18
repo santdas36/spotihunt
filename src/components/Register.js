1c8815b11c1742e91317beb56c7885821ae6e80c
santdas36 <santdas36@gmail.com>
2021-01-19 00:33:03 +0530

    bind input values

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,6 +8,16 @@ function Register() {
 	const [mem1,setMem1] = useState(false);
 	const [mem2, setMem2] = useState(false);
 	
+	const [teamname, setTeamname] = useState('');
+	const [name1, setName1] = useState('');
+	const [email1, setEmail1] = useState('');
+	const [name2, setName2] = useState('');
+	const [email2, setEmail2] = useState('');
+	const [name3, setName3] = useState('');
+	const [email3, setEmail3] = useState('');
+	const [password, setPassword] = useState('');
+	const [contact, setContact] = useState('');
+	
   return(
   <div className="register">
     <ReactPlayer
@@ -44,24 +54,24 @@ function Register() {
 						<div className="input__field">
 							<label for="teamname"><h3>Create a name for your team</h3></label>
 							<span className="info"><InfoOutlinedIcon style={{fontSize: 16}}/> Team name should only contain alphabets, with no spaces and numbers. Be unique. Be creative.</span>
-							<input id="teamname" placeholder="eg: spotihunters" required minlength="3"/>
+							<input id="teamname" placeholder="eg: spotihunters" required minlength="3" value={teamname} onChange={(e) => setTeamname(e.target.value)} />
 						</div>
 						<div className="input__field">
 							<label for="name1">Your Name</label>
-							<input id="name1" placeholder="John Doe" required />
+							<input id="name1" placeholder="John Doe" required value={name1} onChange={(e) => setName1(e.target.value)} />
 						</div>
 						<div className="input__field">
 							<label for="email1">Your Email Address</label>
-							<input id="email1" type="email" placeholder="johndoe@gmail.com" required/>
+							<input id="email1" type="email" placeholder="johndoe@gmail.com" required value={email1} onChange={(e) => setEmail1(e.target.value)} />
 							<span className="info bottom"><InfoOutlinedIcon style={{fontSize: 16}}/> Your team members will be able login to the contest only using this email or the team name itself.</span>
 						</div>
 						<div className="input__field">
 							<label for="password">Pick a Password</label>
-							<input id="password" type="password" placeholder="••••••••" required minlength="8"/>
+							<input id="password" type="password" placeholder="••••••••" required minlength="8" value={password} onChange={(e) => setPassword(e.target.value)}/>
 						</div>
 						<div className="input__field">
 							<label for="contact">Contact Number</label>
-							<input id="contact" type="tel" placeholder="+91 99999 99999" minlength="10" reauired/>
+							<input id="contact" type="tel" placeholder="+91 99999 99999" minlength="10" reauired value={contact} onChange={(e) => setContact(e.target.value)} />
 						</div>
 					</div>
 					<div className="form__split">
@@ -73,22 +83,22 @@ function Register() {
 							<h3>Participant #2</h3>
 								<div className="input__field">
 									<label for="name2">Name</label>
-									<input id="name2" type="text" placeholder="Jane Doe" required={mem1}/>
+									<input id="name2" type="text" placeholder="Jane Doe" required={mem1} value={name2} onChange={(e) => setName2(e.target.value)} />
 								</div>
 								<div className="input__field">
 									<label for="email2">Email Address</label>
-									<input id="email2" type="email" placeholder="janedoe@gmail.com" required={mem1}/>
+									<input id="email2" type="email" placeholder="janedoe@gmail.com" required={mem1} value={email2} onChange={(e) => setEmail2(e.target.value)} />
 								</div>
 							</div>
 							<div onClick={()=>setMem2(true)} className={`member ${mem2 ? 'active' : ''}`} data-text="Add a 3rd participant">
 								<h3>Participant #3</h3>
 								<div className="input__field">
 									<label for="name3">Name</label>
-									<input id="name3" type="text" placeholder="Julien Doe" required={mem2}/>
+									<input id="name3" type="text" placeholder="Julien Doe" required={mem2} value={name3} onChange={(e) => setName3(e.target.value)} />
 								</div>
 							<div className="input__field">
 									<label for="email3">Email Address</label>
-									<input id="email3" type="email" placeholder="juliendoe@gmail.com" required={mem2}/>
+									<input id="email3" type="email" placeholder="juliendoe@gmail.com" required={mem2} value={email3} onChange={(e) => setEmail3(e.target.value)} />
 							</div>
 						</div>
 					</div>
