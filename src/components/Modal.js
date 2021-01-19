4312ac92b6d6cda4458a94276e44e4b7f3de5e42
santdas36 <santdas36@gmail.com>
2021-01-19 13:16:22 +0530

    braces

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,7 @@ import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 function Modal({title, message, close}) {
   return (
     <div className="modal">
-      <div className={modal__header}>
+      <div className="modal__header">
         <h3>{title}</h3>
         <CloseRoundedIcon style={{fontSize: 24}} onClick={close}/>
       </div>
