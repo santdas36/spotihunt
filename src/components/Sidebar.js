69e71509f668150daa230ca9130ac44d93f75108
santdas36 <santdas36@gmail.com>
2021-01-22 13:54:14 +0530

    hints

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import './Sidebar.css';
 import {useEffect, useState} from 'react';
 import SHLogo from '../assets/logo_sh.png';
+import BulbOn from '../assets/bulb_on.png';
+import BulbOff from '../assets/bulb_off.png';
 import {AccessTimeRounded, LockOutlined} from '@material-ui/icons';
 
 function Sidebar() {
@@ -14,6 +16,15 @@ function Sidebar() {
     		<li className="locked"><LockOutlined /> Level 3</li>
     	</ul>
     	<p className="sidebar__info">New levels unlock on completion of current level.</p>
+    	<div className="sidebar__hints">
+    		<h4>Feeling stuck!?</h4>
+    		<p>You are given a total of only three hints for the entire contest. Use wisely.</p>
+    		<div className="sidebar__hintsImg">
+    			<img className="hintUsed" src={BulbOff} />
+    			<img src={BulbOn} />
+    			<img src={BulbOn} />
+    		</div>
+    	</div>
     </div>
   );
 }
