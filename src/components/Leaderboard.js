6cfbb5847ed1f52fa78bdb4f0b1364dfb91fc4f3
santdas36 <santdas36@gmail.com>
2021-01-23 18:22:32 +0530

    header

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -8,6 +8,13 @@ function Leaderboard() {
 	
   return (
     <div className="leaderboard">
+    	<div className="leaderboard__header">
+    		<span className="header__name">
+				<small>YOUR TEAM</small>
+				<p>Spotihunters007</p>
+			</span>
+			<img className="header__icon" src="https://avatars.dicebear.com/4.5/api/gridy/dkso.svg" />
+    	</div>
     	<h3>Leaderboard</h3>
     	<ul className="leaderboard__teams">
     		{
