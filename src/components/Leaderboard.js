b6d9e410fe4ef5057f5f0111076aad4b41a03b85
santdas36 <santdas36@gmail.com>
2021-01-25 16:31:38 +0530

    userdats

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,24 +1,26 @@
 import './Leaderboard.css';
 import {useEffect, useState} from 'react';
 import {ReactComponent as TrophyIcon} from '../assets/trophy.svg';
+import {useStateValue} from '../StateProvider';
 
 function Leaderboard() {
 	const arr = Array.from(Array(20)).map((e,i)=>i+1);
+	const [{user}] = useStateValue();
 	
   return (
     <div className="leaderboard">
     	<div className="leaderboard__header">
     		<span className="header__name">
 				<small>YOUR TEAM</small>
-				<p>Spotihunters007</p>
+				<p>{user?.teamname}</p>
 			</span>
-			<img className="header__icon" src="https://avatars.dicebear.com/4.5/api/gridy/dkso.svg" />
+			<img className="header__icon" src={user?.photoURL} />
     	</div>
     	<h3>Leaderboard</h3>
     	<ul className="leaderboard__teams">
     		{
     			arr.map((el,index)=> (
-    				<li className={index===3 ? 'active' : ''}>
+    				<li className={index===user?.teamname ? 'active' : ''}>
     					{index < 3 ?
     						(<TrophyIcon />) :
     						(<span className="leaderboard__rank">{index+1}</span>)
