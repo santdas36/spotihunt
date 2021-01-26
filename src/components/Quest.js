e9308f246ce636593d385f40dc48ebf751e51aaf
santdas36 <santdas36@gmail.com>
2021-01-26 14:12:40 +0530

    hint

--- src/components/Quest.js
+++ src/components/Quest.js
@@ -9,6 +9,7 @@ function Quest() {
 	const {levelId, questId} = useParams();
 	const [{questions}] = useStateValue();
 	const [answer, setAnswer] = useState('');
+	const [hint, setHint] = useState('');
 	
 	const validate = async (e) => {
 		e.preventDefault();
@@ -17,6 +18,11 @@ function Quest() {
 		if (response > 0.8) {
 			alert(response);
 		}
+	}
+	
+	const getHint = async () => {
+		const response = await fetch(`https://spotihunt-backend.vercel.app/api/get-hint?level=${levelId-1}&quest=${questId-1}&used=1`).then((data) => data.text());
+		setHint(response);
 	}
 	
 	return (
@@ -29,7 +35,7 @@ function Quest() {
 		>
 			<form className="quest__box" onSubmit={(e) => validate(e)}>
 				<p className="quest__question">{levelId}/{questId}{ }{questions && questions[`l${levelId}`][`q${questId}`]}</p>
-				<p className="quest__hint"><img src={HintIcon} /><span>Hint is here duh</span></p>
+				<p className="quest__hint" onClick={getHint}><img src={HintIcon} /><span>{hint}</span></p>
 				<span className="quest__answer">
 					<input type="text" placeholder="Type your answer here..." value={answer} onChange={(e) => setAnswer(e.target.value)} />
 					<button>Submit Answer</button>
