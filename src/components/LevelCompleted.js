b1e59b6a69c93cdd247be9669e1a79955bd786d6
santdas36 <santdas36@gmail.com>
2021-01-27 20:33:27 +0530

    levelId

--- src/components/LevelCompleted.js
+++ src/components/LevelCompleted.js
@@ -13,7 +13,7 @@ function LevelCompleted({time, levelId}) {
     	exit={{y: 'calc(-50% + 5rem)', x: '-50%', opacity: 0}}
     	transition={{type: "spring", duration: 0.5}}
     >
-    	<h3>Level 1 Complete!</h3>
+    	<h3>Level {levelId} Complete!</h3>
 		<p>You have completed Level {levelId} in {time}.<br/>Please continue to Level {levelId + 1}.</p>
 		<button onClick={()=> history.push(`/lvl/${levelId + 1}`)}>Go to Next Level</button>
     </motion.div>
