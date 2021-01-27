19db874f27d1c4f3085e9c917b0b7fc775970ec1
santdas36 <santdas36@gmail.com>
2021-01-27 16:06:12 +0530

    minwidth

--- src/App.js
+++ src/App.js
@@ -9,23 +9,34 @@ import {ToastContainer} from 'react-toastify';
 function App() {
 	const [loaded, setLoaded] = useState(true);
 	const [fadeOut, setFadeOut] = useState(false);
+	const [hasMinWidth, setHasMinWidth] = useState(false);
+	
+	useEffect(()=>{
+		setHasMinWidth(window.matchMedia('(min-width: 1000px)').matches);
+	}, []);
 	
   return (
     <div className="app">
-      {loaded ? 
-      <EventPage /> :
-      <ReactPlayer
-      	className={`intro__video ${fadeOut ? 'fadeOut' : ''}`}
-      	url="https://github.com/santdas36/spot-i-hunt/raw/main/intro.mp4"
-      	width="100%"
-      	height="100%"
-      	playing
-      	muted
-      	onEnded={() => {
-      		setFadeOut(true);
-      		setTimeout(()=> setLoaded(true), 1000)}
-      		}/>
+    
+      {hasMinWidth ?
+      	{loaded ? 
+      	<EventPage /> :
+      	<ReactPlayer
+      		className={`intro__video ${fadeOut ? 'fadeOut' : ''}`}
+      		url="https://github.com/santdas36/spot-i-hunt/raw/main/intro.mp4"
+      		width="100%"
+      		height="100%"
+      		playing
+      		muted
+      		onEnded={() => {
+      			setFadeOut(true);
+      			setTimeout(()=> setLoaded(true), 1000)
+      		}}
+      	/>
+      	} :
+      	(<h1>Small Screen</h1>)
       }
+      
       <ToastContainer position="bottom-right"/>
     </div>
   );
