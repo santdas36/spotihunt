3f185b0113bb808b2f488f190e5861034ea1d82b
santdas36 <santdas36@gmail.com>
2021-01-30 14:16:09 +0530

    sync time

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,6 +6,10 @@ function Timer() {
 	const startDate = new Date('Jan 30, 2021 14:00:00').getTime();
 	
 	useEffect(()=> {
+		const syncTime = async() => {
+			return fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata').then((response) => response.json()).then((response) => new Date(response.datetime).getTime() - new Date().getTime());
+		}
+		console.log('sync', syncTime());
 		const timerInterval = setInterval(()=> {
 			const nowTime = new Date().getTime();
 			const difference = ~~(new Date(startDate - nowTime).getTime() / 1000);
