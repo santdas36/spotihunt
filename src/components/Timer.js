f1d7c0249d1231a712e2f9f2ce2930db85e3f059
santdas36 <santdas36@gmail.com>
2021-01-30 13:35:56 +0530

    timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,19 +2,22 @@ import {useEffect} from 'react';
 import {useStateValue} from '../StateProvider';
 
 function Timer() {
-	const [dispatch] = useStateValue();
+	const [{time}, dispatch] = useStateValue();
 	const startDate = new Date('Jan 30, 2021 14:00:00').getTime();
 	
 	useEffect(()=> {
-		setInterval(()=> {
+		const timerInterval = setInterval(()=> {
 			const nowTime = new Date().getTime();
 			const difference = ~~(new Date(startDate - nowTime).getTime() / 1000);
-			console.log(difference);
 			dispatch({
 				type: 'SET_TIME',
 				time: difference,
 			});
 		}, 1000);
+		
+		return () => {
+			clearInterval(timerInterval);
+		}
 	}, []);
 	
 	return null;
