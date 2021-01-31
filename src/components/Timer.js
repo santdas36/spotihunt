87b58b4080e41e7dae59e81987dc38fc2e7531ac
santdas36 <santdas36@gmail.com>
2021-01-31 15:01:02 +0530

    1510

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,7 +3,7 @@ import {useStateValue} from '../StateProvider';
 
 function Timer() {
 	const [{time}, dispatch] = useStateValue();
-	const startDate = +new Date('2021-01-31T214:30:00.0+05:30');
+	const startDate = +new Date('2021-01-31T15:10:00.0+05:30');
 	const [syncr, setSyncr] = useState(null);
 	
 	useEffect(()=> {
