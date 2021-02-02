e61944b17fcf81f08d0abb4b234bbb34845dac98
santdas36 <santdas36@gmail.com>
2021-02-02 22:25:08 +0530

    videobuttons

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import EventPage from './EventPage';
 import ReactPlayer from 'react-player';
 import {useEffect, useState} from 'react';
 import {ToastContainer} from 'react-toastify';
+import {VolumeOffRounded, VolumeUpRounded} from '@material-ui/icons';
 
 function App() {
 	const [loaded, setLoaded] = useState(false);
+	const [muted, setMuted] = useState(true);
 	const [fadeOut, setFadeOut] = useState(false);
 	const hasMinWidth = window.matchMedia('(min-width: 720px)').matches;
 	
@@ -18,18 +20,23 @@ function App() {
       {hasMinWidth ?
       	(<>{loaded ? 
       	<Register /> :
-      	<ReactPlayer
+      	<><ReactPlayer
       		className={`intro__video ${fadeOut ? 'fadeOut' : ''}`}
       		url="https://github.com/santdas36/spot-i-hunt/raw/main/intro.mp4"
       		width="100%"
       		height="100%"
       		playing
-      		muted
+      		muted={muted}
       		onEnded={() => {
       			setFadeOut(true);
       			setTimeout(()=> setLoaded(true), 1000)
       		}}
       	/>
+      	<div className="videoButtons">
+      		<span onClick={()=> setLoaded(true)}>Skip Intro</span>
+      		<span onClick={()=> setMuted(!muted)}>{muted ? <VolumeOffRounded/> : <VolumeUpRounded/>}</span>
+      	</div>
+      	</>
       	}</>) :
       	(<Unsupported/>)
       }
