11a50d8f74797fa3c16f3dbb07ba2f593dbdeed1
santdas36 <santdas36@gmail.com>
2021-02-12 20:40:44 +0530

    animDelays

--- src/components/EventCompleted.js
+++ src/components/EventCompleted.js
@@ -42,12 +42,12 @@ function EventCompleted() {
     	className="eventCompleted"
     	initial={{opacity: 0}}
     	animate={{opacity: 1}}
-    	transition={{type: "tween", duration: 0.5}}
+    	transition={{type: "tween", duration: 0.5, delay: 0.5}}
     >
-		<motion.h1 initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 0.3}}>Thanks for <br/>Your Participation!</motion.h1>
-		<motion.h2 initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 0.5}}>Stay Tuned for Results.</motion.h2>
+		<motion.h1 initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 1.5}}>Thanks for <br/>Your Participation!</motion.h1>
+		<motion.h2 initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 1.8}}>Stay Tuned for Results.</motion.h2>
 		
-		<motion.form initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 0.6}} onSubmit={sendFeedback}>
+		<motion.form initial={initialAnim} animate={animate} transition={{type: 'tween', delay: 2}} onSubmit={sendFeedback}>
 			{fbSent ? <motion.h3 initial={initialAnim} animate={animate} className="feedbackSent">Thanks for your feedback.</motion.h3> :
 			<><label>Got anything to say to us?</label>
 			<textarea required value={feedback} onChange={(e)=>setFeedback(e.target.value)} rows="3" placeholder="Write about your experience, and how it can be improved in the upcoming events..."></textarea>
@@ -55,7 +55,7 @@ function EventCompleted() {
 		</motion.form>
 
 		
-		<motion.p initial={{y: '-1rem', opacity: 0}} transition={{type: 'tween', delay: 0.5}} animate={animate} ><span>Hope you had fun! - </span><a href="https://instagram.com/christ.keng">@christ.keng</a></motion.p>
+		<motion.p initial={{y: '-1rem', opacity: 0}} transition={{type: 'tween', delay: 1.9}} animate={animate} ><span>Hope you had fun! - </span><a href="https://instagram.com/christ.keng">@christ.keng</a></motion.p>
     </motion.div>
   );
 }
