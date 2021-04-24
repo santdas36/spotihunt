2b885ef184f7631e75a82a74cf6eddd014960aa5
santdas36 <santdas36@gmail.com>
2021-04-25 01:00:39 +0530

    vars 3

--- src/vars.js
+++ src/vars.js
@@ -1,5 +1,7 @@
 import {db} from "./firebase";
 
-export const vars = async () => {
+const vars = async () => {
   return await db.collection('config').get().then((res) => res.docs[0].data())
 }
+
+export { vars }
