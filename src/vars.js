f9b539176f3d434128cf029a23c958e31ba30dae
santdas36 <santdas36@gmail.com>
2021-04-25 00:43:36 +0530

    vars firebase

--- src/vars.js
+++ src/vars.js
@@ -1,7 +1,7 @@
-export const vars = {
-  duration: 3600, // in seconds
-  levels: 3,
-  quests: 5,
-  maxHints: 3,
-  startTime: "2021-03-01T00:00:00.0+05:30",
-};
+import {db} from "./firebase";
+
+const vars = async () => {
+  return await db.collection('config').get().then((res) => res.docs[0].data())
+}
+
+export vars;
